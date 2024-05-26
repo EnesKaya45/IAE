56c09cfc1fe366d2d263359b78fb68bb7853c2a4
Enes <xeneskaya@gmail.com>
2024-05-26 22:09:37 +0300

    Seperated main file in order to make the jar file.

---
+++ src/main/java/com/example/iae/Launcher.java
@@ -0,0 +1,25 @@
+package com.example.iae;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainScreen.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Integrated Assignment Environment");
+        stage.setScene(scene);
+        try{
+            stage.getIcons().add(new Image(getClass().getResourceAsStream("ieu_logo_en_3.jpg")));
+        } catch (Exception e) {
+            System.out.println("Warning: Icon not found.");
+        }
+        stage.show();
+    }
+}
